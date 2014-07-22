7baa612fa8562ab73981d7e65c6f5173bd3b6450
ckkim <ckkim@hardkernel.com>
2014-07-22 15:23:21 +0900

    kit-kat pvr driver modify

--- drivers/gpu/pvr/services4/system/exynos5410/sec_control_pwr_clk.c
+++ drivers/gpu/pvr/services4/system/exynos5410/sec_control_pwr_clk.c
@@ -43,7 +43,7 @@ struct pm_qos_request exynos5_g3d_int_qos;
 static int sec_gpu_top_clock;
 static int gpu_voltage_marin;
 int sec_wakeup_lock_state = 1;
-bool sec_gpu_power_on = false;
+bool sec_gpu_power_on;
 
 module_param(sec_wakeup_lock_state, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 MODULE_PARM_DESC(sec_wakeup_lock_state, "SGX wakeup lock setting");
@@ -99,7 +99,6 @@ void sec_gpu_vol_clk_change(int sgx_clock, int sgx_voltage)
 	sgx_voltage += gpu_voltage_marin;
 #if defined(CONFIG_ARM_EXYNOS5410_BUS_DEVFREQ)
 	if (sec_gpu_power_on) {
-
 	if (sgx_clock >= sec_gpu_top_clock) {
 	#ifdef CONFIG_ARM_EXYNOS_IKS_CPUFREQ
 			pm_qos_update_request(&exynos5_g3d_cpu_qos, 600000);
@@ -117,7 +116,6 @@ void sec_gpu_vol_clk_change(int sgx_clock, int sgx_voltage)
 		pm_qos_update_request(&exynos5_g3d_int_qos, 0);
 		pm_qos_update_request(&exynos5_g3d_mif_qos, 0);
 	}
-
 #endif
 	if (sec_gpu_power_on)	{
 		if (cur_sgx_clock > sgx_clock) {
@@ -133,7 +131,7 @@ void sec_gpu_vol_clk_change(int sgx_clock, int sgx_voltage)
 	else {
 		sec_gpu_setting_clock = sgx_clock;
 		sec_gpu_setting_voltage = sgx_voltage;
-		PVR_LOG(("SGX keep DVFS info sgx_clock:%d MHz, sgx_voltage:%d mV ", sgx_clock, sgx_voltage));
+//		PVR_LOG(("SGX keep DVFS info sgx_clock:%d MHz, sgx_voltage:%d mV ", sgx_clock, sgx_voltage));
 	}
 
 	mutex_unlock(&lock);
