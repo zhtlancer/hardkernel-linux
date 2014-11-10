19c6a28840a06921e35ca0399d5921950dc8bfc7
Spud2233 <spuddie2003@hotmail.com>
2014-11-10 22:13:41 +1100

    Update hid-multitouch.c

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -1075,11 +1075,11 @@ static const struct hid_device_id mt_devices[] = {
     	HID_USB_DEVICE(USB_VENDOR_ID_FOCALTECH, 
     	    USB_DEVICE_ID_FOCALTECH_TOUCH) },
     	    
-    /* Chalkboard Electronics 7" and 10" */
-    { .driver_data = MT_CLS_DEFAULT, HID_USB_DEVICE(0x04D8,0xF724) },
+   /* Chalkboard Electronics 7" and 10" */
+    { .driver_data = MT_CLS_DEFAULT, HID_USB_DEVICE(USB_VENDOR_ID_MICROCHIP,USB_DEVICE_ID_CHALK_ELEC_7_10) },
 
     /* Chalkboard Electronics 14" */
-    { .driver_data = MT_CLS_DEFAULT, HID_USB_DEVICE(0x0EEF,0xA107) },
+    { .driver_data = MT_CLS_DEFAULT, HID_USB_DEVICE(USB_VENDOR_ID_DWAV,USB_DEVICE_ID_CHALK_ELEC_14) },
     
     /* Elitegroup Computer Systems */
     { .driver_data = MT_CLS_DEFAULT,
