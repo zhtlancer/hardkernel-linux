09c049b1df3ac7f3e71ee565b8920a1490da192f
codewalker <codewalker@hardkernel.com>
2015-08-25 15:15:44 +0900

    ODROID-U: Add Xenarc 700CSH multitouch V/PID.
    Change-Id: Ie4d5687c73eb370a28d317cb09dd117ddabea3a7

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -1192,6 +1192,13 @@ static const struct hid_device_id mt_devices[] = {
 		HID_USB_DEVICE(USB_VENDOR_ID_ACER,
 				USB_DEVICE_ID_UT220HQL_TOUCH) },
 
+	 /* Xenarc 700CSH with eGalax Capacitive Multi-touch Screen */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_XENARC,
+				USB_DEVICE_ID_700CSH_TOUCH) },
+
+	/* Generic MT device */
+	{ HID_DEVICE(HID_BUS_ANY, HID_GROUP_MULTITOUCH, HID_ANY_ID, HID_ANY_ID) },
 	{ }
 };
 MODULE_DEVICE_TABLE(hid, mt_devices);
