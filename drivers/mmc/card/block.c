e86f0a8a9e377a983e3ac962a6f7c0b8733a3424
ckkim <ckkim@hardkernel.com>
2015-09-07 18:27:48 +0900

    ODROID-C2: set boot device as always mmcblk0
    Change-Id: Id7daf24f97cc11cc6c3c9ce57c4722caef69e2a2

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -49,6 +49,10 @@
 #include <linux/mmc/emmc_partitions.h>
 #endif
 
+#if defined(CONFIG_ARCH_MESON64_ODROIDC2)
+#include <linux/hardkernel/odroidc2.h>
+#endif
+
 MODULE_ALIAS("mmc:block");
 #ifdef MODULE_PARAM_PREFIX
 #undef MODULE_PARAM_PREFIX
@@ -2098,12 +2102,22 @@ static struct mmc_blk_data *mmc_blk_alloc_req(struct mmc_card *card,
 	 * partitions, devidx will not coincide with a per-physical card
 	 * index anymore so we keep track of a name index.
 	 */
+#if defined(CONFIG_ARCH_MESON64_ODROIDC2)
+	if (strncmp(dev_name(&card->host->class_dev), "sd", 2) == 0) {
+		md->name_idx = board_boot_from_emmc() ? 1 : 0;
+		__set_bit(md->name_idx, name_use);
+	} else if (strncmp(dev_name(&card->host->class_dev), "emmc", 4) == 0) {
+		md->name_idx = board_boot_from_emmc() ? 0 : 1;
+		__set_bit(md->name_idx, name_use);
+	}
+#else
 	if (!subname) {
 		md->name_idx = find_first_zero_bit(name_use, max_devices);
 		__set_bit(md->name_idx, name_use);
 	} else
 		md->name_idx = ((struct mmc_blk_data *)
 				dev_to_disk(parent)->private_data)->name_idx;
+#endif
 
 	md->area_type = area_type;
 
