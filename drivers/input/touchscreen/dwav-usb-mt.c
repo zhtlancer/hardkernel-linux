9e9be708679165fd5dd81263de1f032fdf831f1a
codewalker <codewalker@hardkernel.com>
2015-09-30 15:18:40 +0900

    ODROID-C1: Fix to DWAV touch screen.
    Change-Id: I81d8ed21cdd947f1707a2632b0a0be039fcea564

--- drivers/input/touchscreen/dwav-usb-mt.c
+++ drivers/input/touchscreen/dwav-usb-mt.c
@@ -22,7 +22,7 @@
 #define DWAV_TOUCH_MAX_PRESSURE     255
 
 //[*]-------------------------------------------------------------------------[*]
-#define USB_VENDOR_ID_DWAV		        0x0eef
+#define USB_VENDOR_ID_DWAV				0x0eef
 #define USB_DEVICE_ID_DWAV_MULTITOUCH   0x0005
 
 static const struct usb_device_id dwav_usb_mt_devices[] = {
@@ -32,21 +32,21 @@ static const struct usb_device_id dwav_usb_mt_devices[] = {
 
 //[*]-------------------------------------------------------------------------[*]
 struct dwav_raw {               /* Total 25 bytes */
-    unsigned char   header;     /* frame header 0xAA*/
-    unsigned char   press;      /* Touch flag (1:valid touch data, 0:touch finished) */
-    unsigned short  x1;         /* 1st x */
-    unsigned short  y1;         /* 1st y */
-    unsigned char   end;        /* 1st touch finish flags 0xBB, RPI only uses the first 7 bytes */
-    unsigned char   ids;        /* touch ID(bit field) */
-    unsigned short  x2;
-    unsigned short  y2;
-    unsigned short  x3;
-    unsigned short  y3;
-    unsigned short  x4;
-    unsigned short  y4;
-    unsigned short  x5;
-    unsigned short  y5;
-    unsigned char   tail;       /* frame end 0xCC */
+	unsigned char   header;     /* frame header 0xAA*/
+	unsigned char   press;      /* Touch flag (1:valid touch data, 0:touch finished) */
+	unsigned short  x1;         /* 1st x */
+	unsigned short  y1;         /* 1st y */
+	unsigned char   end;        /* 1st touch finish flags 0xBB, RPI only uses the first 7 bytes */
+	unsigned char   ids;        /* touch ID(bit field) */
+	unsigned short  y2;
+	unsigned short  x2;
+	unsigned short  y3;
+	unsigned short  x3;
+	unsigned short  y4;
+	unsigned short  x4;
+	unsigned short  y5;
+	unsigned short  x5;
+	unsigned char   tail;       /* frame end 0xCC */
 };
 
 //[*]-------------------------------------------------------------------------[*]
@@ -67,7 +67,7 @@ typedef	struct	finger__t	{
 struct dwav_usb_mt  {
 	char            name[128], phys[64];
 
-    // for URB Data DMA
+	// for URB Data DMA
 	dma_addr_t      data_dma;
 	unsigned char   *data;
 	int             data_size;
@@ -82,72 +82,75 @@ struct dwav_usb_mt  {
 //[*]-------------------------------------------------------------------------[*]
 static void dwav_usb_mt_report(struct dwav_usb_mt *dwav_usb_mt)
 {
-	int		id;
+	int	id;
 
 	for(id = 0; id < DWAV_TOUCH_MAX_ID; id++)	{
 
 		if(dwav_usb_mt->finger[id].status == TS_EVENT_UNKNOWN)  continue;
 
-    	input_mt_slot(dwav_usb_mt->input, id);
+		if( dwav_usb_mt->finger[id].x >= DWAV_TOUCH_MAX_X ||
+			dwav_usb_mt->finger[id].y >= DWAV_TOUCH_MAX_Y  )	continue;
+
+		input_mt_slot(dwav_usb_mt->input, id);
 
 		if(dwav_usb_mt->finger[id].status != TS_EVENT_RELEASE)	{
-            input_mt_report_slot_state(dwav_usb_mt->input, MT_TOOL_FINGER, true);
+			input_mt_report_slot_state(dwav_usb_mt->input, MT_TOOL_FINGER, true);
 			input_report_abs(dwav_usb_mt->input, ABS_MT_POSITION_X,  dwav_usb_mt->finger[id].x);
 			input_report_abs(dwav_usb_mt->input, ABS_MT_POSITION_Y,	 dwav_usb_mt->finger[id].y);
 			input_report_abs(dwav_usb_mt->input, ABS_MT_PRESSURE, DWAV_TOUCH_MAX_PRESSURE);
 		}
 		else	{
-            input_mt_report_slot_state(dwav_usb_mt->input, MT_TOOL_FINGER, false);
-		    dwav_usb_mt->finger[id].status = TS_EVENT_UNKNOWN;
+			input_mt_report_slot_state(dwav_usb_mt->input, MT_TOOL_FINGER, false);
+			dwav_usb_mt->finger[id].status = TS_EVENT_UNKNOWN;
 		}
 		input_mt_report_pointer_emulation(dwav_usb_mt->input, true);
-	    input_sync(dwav_usb_mt->input);
+		input_sync(dwav_usb_mt->input);
 	}
 }
 
 //[*]-------------------------------------------------------------------------[*]
 static void dwav_usb_mt_process(struct dwav_usb_mt *dwav_usb_mt,
-                                   unsigned char *pkt, int len)
+		unsigned char *pkt, int len)
 {
-    struct  dwav_raw    *dwav_raw = (struct dwav_raw *)pkt;
-    unsigned char       bit_mask, cnt;
+	struct  dwav_raw *dwav_raw = (struct dwav_raw *)pkt;
+	unsigned char bit_mask, cnt;
 
-    for(cnt = 0, bit_mask = 0x01; cnt < DWAV_TOUCH_MAX_ID; cnt++, bit_mask <<= 1)   {
-        if((dwav_raw->ids & bit_mask) && dwav_raw->press)    {
-            dwav_usb_mt->finger[cnt].status = TS_EVENT_PRESS;
-            switch(cnt) {
-                case    0:
-                    dwav_usb_mt->finger[cnt].x = cpu_to_be16(dwav_raw->x1);
-                    dwav_usb_mt->finger[cnt].y = cpu_to_be16(dwav_raw->y1);
-                    break;
-                case    1:
-                    dwav_usb_mt->finger[cnt].x = cpu_to_be16(dwav_raw->x2);
-                    dwav_usb_mt->finger[cnt].y = cpu_to_be16(dwav_raw->y2);
-                    break;
-                case    2:
-                    dwav_usb_mt->finger[cnt].x = cpu_to_be16(dwav_raw->x3);
-                    dwav_usb_mt->finger[cnt].y = cpu_to_be16(dwav_raw->y3);
-                    break;
-                case    3:
-                    dwav_usb_mt->finger[cnt].x = cpu_to_be16(dwav_raw->x4);
-                    dwav_usb_mt->finger[cnt].y = cpu_to_be16(dwav_raw->y4);
-                    break;
-                case    4:
-                    dwav_usb_mt->finger[cnt].x = cpu_to_be16(dwav_raw->x5);
-                    dwav_usb_mt->finger[cnt].y = cpu_to_be16(dwav_raw->y5);
-                    break;
-                default :
-                    break;
-            }
-        }
-        else    {
-            if(dwav_usb_mt->finger[cnt].status == TS_EVENT_PRESS)
-                dwav_usb_mt->finger[cnt].status = TS_EVENT_RELEASE;
-            else
-                dwav_usb_mt->finger[cnt].status = TS_EVENT_UNKNOWN;
-        }
-    }
-    dwav_usb_mt_report(dwav_usb_mt);
+	for(cnt = 0, bit_mask = 0x01; cnt < DWAV_TOUCH_MAX_ID; cnt++, bit_mask <<= 1)   {
+		if((dwav_raw->ids & bit_mask) && dwav_raw->press)	{
+			dwav_usb_mt->finger[cnt].status = TS_EVENT_PRESS;
+			switch(cnt) {
+				case	0:
+					dwav_usb_mt->finger[cnt].x = cpu_to_be16(dwav_raw->x1);
+					dwav_usb_mt->finger[cnt].y = cpu_to_be16(dwav_raw->y1);
+					break;
+				case	1:
+					dwav_usb_mt->finger[cnt].x = cpu_to_be16(dwav_raw->x2);
+					dwav_usb_mt->finger[cnt].y = cpu_to_be16(dwav_raw->y2);
+					break;
+				case	2:
+					dwav_usb_mt->finger[cnt].x = cpu_to_be16(dwav_raw->x3);
+					dwav_usb_mt->finger[cnt].y = cpu_to_be16(dwav_raw->y3);
+					break;
+				case	3:
+					dwav_usb_mt->finger[cnt].x = cpu_to_be16(dwav_raw->x4);
+					dwav_usb_mt->finger[cnt].y = cpu_to_be16(dwav_raw->y4);
+					break;
+				case	4:
+					dwav_usb_mt->finger[cnt].x = cpu_to_be16(dwav_raw->x5);
+					dwav_usb_mt->finger[cnt].y = cpu_to_be16(dwav_raw->y5);
+					break;
+				default :
+					break;
+			}
+		}
+		else	{
+			if(dwav_usb_mt->finger[cnt].status == TS_EVENT_PRESS)
+				dwav_usb_mt->finger[cnt].status = TS_EVENT_RELEASE;
+			else
+				dwav_usb_mt->finger[cnt].status = TS_EVENT_UNKNOWN;
+		}
+	}
+	dwav_usb_mt_report(dwav_usb_mt);
 }
 
 //[*]-------------------------------------------------------------------------[*]
@@ -158,26 +161,26 @@ static void dwav_usb_mt_irq(struct urb *urb)
 	int retval;
 
 	switch (urb->status) {
-    	case 0:
-    		/* success */
-    		break;
-    	case -ETIME:
-    		/* this urb is timing out */
-    		dev_dbg(dev, "%s - urb timed out - was the device unplugged?\n", __func__);
-    		return;
-    	case -ECONNRESET:
-    	case -ENOENT:
-    	case -ESHUTDOWN:
-    	case -EPIPE:
-    		/* this urb is terminated, clean up */
-    		dev_dbg(dev, "%s - urb shutting down with status: %d\n", __func__, urb->status);
-    		return;
-    	default:
-    		dev_dbg(dev, "%s - nonzero urb status received: %d\n", __func__, urb->status);
-    		goto exit;
+		case 0:
+			/* success */
+			break;
+		case -ETIME:
+			/* this urb is timing out */
+			dev_dbg(dev, "%s - urb timed out - was the device unplugged?\n", __func__);
+			return;
+		case -ECONNRESET:
+		case -ENOENT:
+		case -ESHUTDOWN:
+		case -EPIPE:
+			/* this urb is terminated, clean up */
+			dev_dbg(dev, "%s - urb shutting down with status: %d\n", __func__, urb->status);
+			return;
+		default:
+			dev_dbg(dev, "%s - nonzero urb status received: %d\n", __func__, urb->status);
+			goto exit;
 	}
 
-    dwav_usb_mt_process(dwav_usb_mt, dwav_usb_mt->data, urb->actual_length);
+	dwav_usb_mt_process(dwav_usb_mt, dwav_usb_mt->data, urb->actual_length);
 
 exit:
 	usb_mark_last_busy(interface_to_usbdev(dwav_usb_mt->interface));
@@ -271,12 +274,11 @@ static void dwav_usb_mt_free_buffers(struct usb_device *udev,
 				  struct dwav_usb_mt *dwav_usb_mt)
 {
 	usb_free_coherent(udev, dwav_usb_mt->data_size,
-			  dwav_usb_mt->data, dwav_usb_mt->data_dma);
+			dwav_usb_mt->data, dwav_usb_mt->data_dma);
 }
 
 //[*]-------------------------------------------------------------------------[*]
-static struct usb_endpoint_descriptor *
-    dwav_usb_mt_get_input_endpoint(struct usb_host_interface *interface)
+static struct usb_endpoint_descriptor *dwav_usb_mt_get_input_endpoint(struct usb_host_interface *interface)
 {
 	int i;
 
@@ -291,24 +293,20 @@ static struct usb_endpoint_descriptor *
 //[*]-------------------------------------------------------------------------[*]
 static int dwav_usb_mt_init (struct dwav_usb_mt *dwav_usb_mt, void *dev)
 {
-    int err;
-    struct input_dev *input_dev = (struct input_dev *)dev;
+	int err;
+	struct input_dev *input_dev = (struct input_dev *)dev;
 
 	input_dev->name = dwav_usb_mt->name;
 	input_dev->phys = dwav_usb_mt->phys;
 
 	input_set_drvdata(input_dev, dwav_usb_mt);
 
-	input_dev->open     = dwav_usb_mt_open;
-	input_dev->close    = dwav_usb_mt_close;
+	input_dev->open = dwav_usb_mt_open;
+	input_dev->close = dwav_usb_mt_close;
 
-	input_dev->id.bustype 	= BUS_USB;
+	input_dev->id.bustype	= BUS_USB;
 
-	input_dev->id.vendor 	= 0x0EEE;
-	input_dev->id.product 	= 0x0005;
-	input_dev->id.version 	= 0x0101;
-
-    // single touch
+	// single touch
 	input_dev->evbit[0] = BIT_MASK(EV_KEY) | BIT_MASK(EV_ABS);
 	input_dev->keybit[BIT_WORD(BTN_TOUCH)] = BIT_MASK(BTN_TOUCH);
 
@@ -322,12 +320,12 @@ static int dwav_usb_mt_init (struct dwav_usb_mt *dwav_usb_mt, void *dev)
 
 	if ((err = input_register_device(input_dev))) {
 		pr_err("%s - input_register_device failed, err: %d\n", __func__, err);
-        return  err;
+		return  err;
 	}
 
 	dwav_usb_mt->input = input_dev;
 
-    return  0;
+	return  0;
 }
 
 //[*]-------------------------------------------------------------------------[*]
@@ -341,21 +339,21 @@ static int dwav_usb_mt_probe(struct usb_interface *intf,
 
 	int err = 0;
 
-    if(!(endpoint = dwav_usb_mt_get_input_endpoint(intf->cur_altsetting)))
-        return  -ENXIO;
+	if(!(endpoint = dwav_usb_mt_get_input_endpoint(intf->cur_altsetting)))
+		return  -ENXIO;
 
-    if(!(dwav_usb_mt = kzalloc(sizeof(struct dwav_usb_mt), GFP_KERNEL)))
-        return  -ENOMEM;
+	if(!(dwav_usb_mt = kzalloc(sizeof(struct dwav_usb_mt), GFP_KERNEL)))
+		return  -ENOMEM;
 
 	if(!(dwav_usb_mt->finger = kzalloc(sizeof(finger_t) * DWAV_TOUCH_MAX_ID , GFP_KERNEL)))
-        goto    err_free_mem;
+		goto err_free_mem;
 
-    if(!(input_dev = input_allocate_device()))
-        goto    err_free_mem;
+	if(!(input_dev = input_allocate_device()))
+		goto err_free_mem;
 
-    dwav_usb_mt->data_size  = sizeof(struct dwav_raw);
-	dwav_usb_mt->data       = usb_alloc_coherent(udev, dwav_usb_mt->data_size,
-					                                GFP_KERNEL, &dwav_usb_mt->data_dma);
+	dwav_usb_mt->data_size = sizeof(struct dwav_raw);
+	dwav_usb_mt->data = usb_alloc_coherent(udev, dwav_usb_mt->data_size,
+			GFP_KERNEL, &dwav_usb_mt->data_dma);
 	if (!dwav_usb_mt->data)
 		goto err_free_mem;
 
@@ -366,11 +364,11 @@ static int dwav_usb_mt_probe(struct usb_interface *intf,
 
 	if (usb_endpoint_type(endpoint) == USB_ENDPOINT_XFER_INT)   {
 		usb_fill_int_urb(dwav_usb_mt->irq, udev,
-		    usb_rcvintpipe(udev, endpoint->bEndpointAddress),
-		    dwav_usb_mt->data, dwav_usb_mt->data_size,
-		    dwav_usb_mt_irq, dwav_usb_mt, endpoint->bInterval);
+			usb_rcvintpipe(udev, endpoint->bEndpointAddress),
+			dwav_usb_mt->data, dwav_usb_mt->data_size,
+			dwav_usb_mt_irq, dwav_usb_mt, endpoint->bInterval);
 	}
-	else    {
+	else {
 		usb_fill_bulk_urb(dwav_usb_mt->irq, udev,
 			 usb_rcvbulkpipe(udev, endpoint->bEndpointAddress),
 			 dwav_usb_mt->data, dwav_usb_mt->data_size,
@@ -395,9 +393,9 @@ static int dwav_usb_mt_probe(struct usb_interface *intf,
 
 	if (!strlen(dwav_usb_mt->name)) {
 		snprintf(dwav_usb_mt->name, sizeof(dwav_usb_mt->name),
-        			"D-WAV Scientific MultiTouch %04x:%04x",
-        			 le16_to_cpu(udev->descriptor.idVendor),
-        			 le16_to_cpu(udev->descriptor.idProduct));
+				"D-WAV Scientific MultiTouch %04x:%04x",
+				le16_to_cpu(udev->descriptor.idVendor),
+				le16_to_cpu(udev->descriptor.idProduct));
 	}
 
 	usb_make_path(udev, dwav_usb_mt->phys, sizeof(dwav_usb_mt->phys));
@@ -407,8 +405,8 @@ static int dwav_usb_mt_probe(struct usb_interface *intf,
 
 	input_dev->dev.parent = &intf->dev;
 
-    if((err = dwav_usb_mt_init (dwav_usb_mt, (void *)input_dev)))
-        goto    err_free_urb;
+	if((err = dwav_usb_mt_init (dwav_usb_mt, (void *)input_dev)))
+		goto err_free_urb;
 
 	usb_set_intfdata(intf, dwav_usb_mt);
 
@@ -418,10 +416,10 @@ err_free_urb:
 	usb_free_urb(dwav_usb_mt->irq);
 
 err_free_buffers:
-    dwav_usb_mt_free_buffers(udev, dwav_usb_mt);
+	dwav_usb_mt_free_buffers(udev, dwav_usb_mt);
 
 err_free_mem:
-    if(input_dev)   input_free_device(input_dev);
+	if(input_dev)   input_free_device(input_dev);
 	if(dwav_usb_mt) kfree(dwav_usb_mt);
 
 	return err;
@@ -453,13 +451,13 @@ MODULE_DEVICE_TABLE(usb, dwav_usb_mt_devices);
 
 //[*]-------------------------------------------------------------------------[*]
 static struct usb_driver dwav_usb_mt_driver = {
-	.name		    = "dwav_usb_mt",
-	.probe		    = dwav_usb_mt_probe,
-	.disconnect	    = dwav_usb_mt_disconnect,
-	.suspend	    = dwav_usb_mt_suspend,
-	.resume		    = dwav_usb_mt_resume,
+	.name			= "dwav_usb_mt",
+	.probe			= dwav_usb_mt_probe,
+	.disconnect		= dwav_usb_mt_disconnect,
+	.suspend		= dwav_usb_mt_suspend,
+	.resume			= dwav_usb_mt_resume,
 	.reset_resume	= dwav_usb_mt_reset_resume,
-	.id_table	    = dwav_usb_mt_devices,
+	.id_table		= dwav_usb_mt_devices,
 	.supports_autosuspend = 1,
 };
 
