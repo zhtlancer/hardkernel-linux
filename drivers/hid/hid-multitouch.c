20618c1028255fd251d78e0588006bd5c31695e4
codewalker <codewalker@hardkernel.com>
2015-11-26 17:56:37 +0900

    ODROID-C1: Add multitouch p/vids.
    /* Dell P2314T USB capacitive Screen */
    USB_VENDOR_ID_DELL	I0x2149
    USB_DEVICE_ID_P2314T_TOUCH	I0x2316
    
    USB_VENDOR_ID_IRTOUCHSYSTEMS	0x6615
    USB_DEVICE_ID_IRTOUCH_TOUCH	0x0001
    
    Change-Id: I64cd5f71d14f5e851bb547ffe7ae899038cbbc18

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -1273,6 +1273,9 @@ static const struct hid_device_id mt_devices[] = {
 	{ .driver_data = MT_CLS_DUAL_INRANGE_CONTACTID,
 		MT_USB_DEVICE(USB_VENDOR_ID_IRTOUCHSYSTEMS,
 			USB_DEVICE_ID_IRTOUCH_INFRARED_USB) },
+	{ .driver_data = MT_CLS_DUAL_INRANGE_CONTACTID,
+		HID_USB_DEVICE(USB_VENDOR_ID_IRTOUCHSYSTEMS,
+			USB_DEVICE_ID_IRTOUCH_TOUCH) },
 
 	/* LG Display panels */
 	{ .driver_data = MT_CLS_DEFAULT,
@@ -1468,6 +1471,10 @@ static const struct hid_device_id mt_devices[] = {
 		HID_USB_DEVICE(USB_VENDOR_ID_SINGWAY,
 				USB_DEVICE_ID_XWC1359_7_TOUCH) },
 
+	 /* Dell P2314T USB capacitive Screen */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_DELL,
+				USB_DEVICE_ID_P2314T_TOUCH) },
 
 	/* Generic MT device */
 	{ HID_DEVICE(HID_BUS_ANY, HID_GROUP_MULTITOUCH, HID_ANY_ID, HID_ANY_ID) },
