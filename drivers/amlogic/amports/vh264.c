4dbab4c4b421829e939a0fa4951417e83a61fa9b
Jian Wang <jian.wang@amlogic.com>
2016-02-01 03:29:09 -0500

    PD#118678: amvideocap: backport amvideocap changes from 3.10
    1. CONFIG_AM_VIDEOCAPTURE=y
    2. add type_original to get true i/p type
    3. move ge2d context create to amvideocap init
    
    Change-Id: I162a2d3b263e226799f661257ecd5c4727a14dfa

--- drivers/amlogic/amports/vh264.c
+++ drivers/amlogic/amports/vh264.c
@@ -1551,6 +1551,7 @@ static void vh264_isr(void)
 				vf->pts_us64 = div64_u64(((u64)vf->pts)*100, 9);
 				vf->canvas0Addr = vf->canvas1Addr =
 					spec2canvas(&buffer_spec[buffer_index]);
+				vf->type_original = vf->type;
 				vfbuf_use[buffer_index]++;
 
 				if ((error_recovery_mode_use & 2) && error) {
@@ -1595,6 +1596,7 @@ static void vh264_isr(void)
 				vf->pts_us64 = div64_u64(((u64)vf->pts)*100, 9);
 				vf->canvas0Addr = vf->canvas1Addr =
 					spec2canvas(&buffer_spec[buffer_index]);
+				vf->type_original = vf->type;
 				vfbuf_use[buffer_index]++;
 				vf->ready_jiffies64 = jiffies_64;
 
@@ -1647,6 +1649,7 @@ static void vh264_isr(void)
 
 				vf->canvas0Addr = vf->canvas1Addr =
 					spec2canvas(&buffer_spec[buffer_index]);
+				vf->type_original = vf->type;
 				vfbuf_use[buffer_index]++;
 
 				p_last_vf = vf;
