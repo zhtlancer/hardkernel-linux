41124682529c80d880993276110c286d0a41dd38
codewalker <codewalker@hardkernel.com>
2016-03-07 10:38:02 +0900

    ODROID-U/X2: Add multitouch V/PID.
    /* Lilliput 779GL-70NP/C/T - 7" HDMI capacitive Screen */
    define USB_VENDOR_ID_LILLIPUT 0x1391
    define USB_DEVICE_ID_LILLIPUT_779GL_TOUCH     0x2112
    
    Change-Id: I3405605cf88faa4de7867c039692cde14749c6a2

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -1241,6 +1241,12 @@ static const struct hid_device_id mt_devices[] = {
 		HID_USB_DEVICE(USB_VENDOR_ID_LG_23ET63V,
 				USB_DEVICE_ID_LG_23ET63V_TOUCH) },
 
+/* Lilliput 779GL-70NP/C/T - 7" HDMI capacitive Screen */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_LILLIPUT,
+				USB_DEVICE_ID_LILLIPUT_779GL_TOUCH) },
+
+
 	{ }
 };
 MODULE_DEVICE_TABLE(hid, mt_devices);
