f29424fde63ef192348370d7e82bb18b3d8fea6d
charles.park <charles.park@hardkernel.com>
2016-04-06 14:34:37 +0900

    ODROID-C2 : HPD disable added.(always vout is hdmi)
    Change-Id: I06f66250b46ba5f9e7e81c84779db32b14d56f96

--- drivers/amlogic/display/logo/logo.c
+++ drivers/amlogic/display/logo/logo.c
@@ -195,8 +195,11 @@ static int refresh_mode_and_logo(bool first)
 {
 	enum vmode_e cur_mode = VMODE_MAX;
 	int cnt = sizeof(mode_infos) / sizeof(mode_infos[0]);
+#if defined(CONFIG_ARCH_MESON64_ODROIDC2)
+	int hdp_state = 1;
+#else
 	int hdp_state = get_hpd_state();
-
+#endif
 	if (!first && osd_get_logo_index() != logo_info.index)
 		return -1;
 
