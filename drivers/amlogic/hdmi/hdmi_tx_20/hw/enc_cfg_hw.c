51c0b3a8f7299e4f5fce030f90bddb7a73279fc8
Joy Cho <joy.cho@hardkernel.com>
2016-05-23 16:19:25 +0900

    ODROID-C2: HDMI PHY: Add resolution 2560x1440 / 2560x1600 / 2560x1080
    Change-Id: I14b79c9801ef0ee388b37266bb53a1a1d38660a8

--- drivers/amlogic/hdmi/hdmi_tx_20/hw/enc_cfg_hw.c
+++ drivers/amlogic/hdmi/hdmi_tx_20/hw/enc_cfg_hw.c
@@ -905,6 +905,84 @@ static const struct reg_s tvregs_vesa_1920x1200p60hz[] = {
 	{MREG_END_MARKER, 0}
 };
 
+static const struct reg_s tvregs_2560x1440p60hz[] = {
+	{P_VENC_VDAC_SETTING, 0xff,},
+	{P_ENCP_VIDEO_EN, 0,},
+	{P_ENCI_VIDEO_EN, 0,},
+
+	{P_ENCP_VIDEO_MODE, 0x4040,},
+	{P_ENCP_VIDEO_MODE_ADV, 0x18,},
+
+	{P_ENCP_VIDEO_MAX_PXCNT, 2719,},
+	{P_ENCP_VIDEO_MAX_LNCNT, 1480,},
+	{P_ENCP_VIDEO_HAVON_BEGIN, 80,},
+	{P_ENCP_VIDEO_HAVON_END, 2639,},
+	{P_ENCP_VIDEO_VAVON_BLINE, 34,},
+	{P_ENCP_VIDEO_VAVON_ELINE, 1473,},
+	{P_ENCP_VIDEO_HSO_BEGIN, 0,},
+	{P_ENCP_VIDEO_HSO_END, 32,},
+	{P_ENCP_VIDEO_VSO_BEGIN, 0x1E,},
+	{P_ENCP_VIDEO_VSO_END, 0x32,},
+	{P_ENCP_VIDEO_VSO_BLINE, 0x0,},
+	{P_ENCP_VIDEO_VSO_ELINE, 0x5,},
+
+	{P_ENCP_VIDEO_EN, 1,},
+	{P_ENCI_VIDEO_EN, 0,},
+	{MREG_END_MARKER, 0},
+};
+
+static const struct reg_s tvregs_2560x1600p60hz[] = {
+	{P_VENC_VDAC_SETTING, 0xff,},
+	{P_ENCP_VIDEO_EN, 0,},
+	{P_ENCI_VIDEO_EN, 0,},
+
+	{P_ENCP_VIDEO_MODE, 0x4040,},
+	{P_ENCP_VIDEO_MODE_ADV, 0x18,},
+
+	{P_ENCP_VIDEO_MAX_PXCNT, 2719,},
+	{P_ENCP_VIDEO_MAX_LNCNT, 1645,},
+	{P_ENCP_VIDEO_HAVON_BEGIN, 80,},
+	{P_ENCP_VIDEO_HAVON_END, 2639,},
+	{P_ENCP_VIDEO_VAVON_BLINE, 38,},
+	{P_ENCP_VIDEO_VAVON_ELINE, 1637,},
+	{P_ENCP_VIDEO_HSO_BEGIN, 0,},
+	{P_ENCP_VIDEO_HSO_END, 32,},
+	{P_ENCP_VIDEO_VSO_BEGIN, 0x1E,},
+	{P_ENCP_VIDEO_VSO_END, 0x32,},
+	{P_ENCP_VIDEO_VSO_BLINE, 0x0,},
+	{P_ENCP_VIDEO_VSO_ELINE, 0x6,},
+
+	{P_ENCP_VIDEO_EN, 1,},
+	{P_ENCI_VIDEO_EN, 0,},
+	{MREG_END_MARKER, 0},
+};
+
+static const struct reg_s tvregs_2560x1080p60hz[] = {
+	{P_VENC_VDAC_SETTING, 0xff,},
+	{P_ENCP_VIDEO_EN, 0,},
+	{P_ENCI_VIDEO_EN, 0,},
+
+	{P_ENCP_VIDEO_MODE, 0x4040,},
+	{P_ENCP_VIDEO_MODE_ADV, 0x18,},
+
+	{P_ENCP_VIDEO_MAX_PXCNT, 2783,},
+	{P_ENCP_VIDEO_MAX_LNCNT, 1110,},
+	{P_ENCP_VIDEO_HAVON_BEGIN, 96,},
+	{P_ENCP_VIDEO_HAVON_END, 2655,},
+	{P_ENCP_VIDEO_VAVON_BLINE, 18,},
+	{P_ENCP_VIDEO_VAVON_ELINE, 1097,},
+	{P_ENCP_VIDEO_HSO_BEGIN, 0,},
+	{P_ENCP_VIDEO_HSO_END, 64,},
+	{P_ENCP_VIDEO_VSO_BEGIN, 0x1E,},
+	{P_ENCP_VIDEO_VSO_END, 0x32,},
+	{P_ENCP_VIDEO_VSO_BLINE, 0x0,},
+	{P_ENCP_VIDEO_VSO_ELINE, 0xa,},
+
+	{P_ENCP_VIDEO_EN, 1,},
+	{P_ENCI_VIDEO_EN, 0,},
+	{MREG_END_MARKER, 0},
+};
+
 struct vic_tvregs_set {
 	enum hdmi_vic vic;
 	const struct reg_s *reg_setting;
@@ -948,6 +1026,9 @@ static struct vic_tvregs_set tvregsTab[] = {
 	{HDMIV_1600x900p60hz, tvregs_vesa_1600x900p60hz},
 	{HDMIV_1680x1050p60hz, tvregs_vesa_1680x1050p60hz},
 	{HDMIV_1920x1200p60hz, tvregs_vesa_1920x1200p60hz},
+	{HDMIV_2560x1440p60hz, tvregs_2560x1440p60hz},
+	{HDMIV_2560x1600p60hz, tvregs_2560x1600p60hz},
+	{HDMIV_2560x1080p60hz, tvregs_2560x1080p60hz},
 };
 
 static inline void setreg(const struct reg_s *r)
