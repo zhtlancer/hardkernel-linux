723319ea8465e48a85dc0b9f46170728bf7f1a68
codewalker <codewalker@hardkernel.com>
2017-11-28 10:49:56 +0900

    ODROID-N1: Add poweroff_trigger sysfs node.
    Change-Id: Ifebd0a6ffb07f8a0dcf3c89a1129396d195cc667

--- drivers/hardkernel/odroid-sysfs.c
+++ drivers/hardkernel/odroid-sysfs.c
@@ -28,7 +28,9 @@
 #include <linux/platform_device.h>
 #include <linux/irq.h>
 #include <linux/sysfs.h>
+#include <linux/input.h>
 #include <linux/io.h>
+#include <linux/hrtimer.h>
 #if defined(CONFIG_HAS_WAKELOCK)
 #include <linux/wakelock.h>
 #endif
@@ -43,6 +45,35 @@ static int boot_mode;
 static struct wake_lock	sleep_wake_lock;
 #endif
 
+static struct hrtimer input_timer;
+static struct input_dev *input_dev;
+static int keycode[] = { KEY_POWER, };
+static int key_release_seconds;
+
+static ssize_t set_poweroff_trigger(struct class *class,
+		struct class_attribute *attr, const char *buf, size_t count)
+{
+	unsigned int val;
+
+	if (0 == sscanf(buf, "%d\n", &val))
+		return  -EINVAL;
+
+	/* Emulate power button by software */
+	if ((val != 0) && (val < 5)) {
+		if (!key_release_seconds) {
+			key_release_seconds = val;
+			input_report_key(input_dev, KEY_POWER, 1);
+
+			hrtimer_start(&input_timer,
+					ktime_set(key_release_seconds, 0),
+					HRTIMER_MODE_REL);
+
+			input_sync(input_dev);
+		}
+	}
+
+	return count;
+}
 /*
  * Discover the boot device within MicroSD or eMMC
  * and return 1 for eMMC, otherwise 0.
@@ -102,6 +133,7 @@ static int __init setup_boot_mode(char *str)
 __setup("storagemedia=", setup_boot_mode);
 
 static struct class_attribute odroid_class_attrs[] = {
+	__ATTR(poweroff_trigger, 0220, NULL, set_poweroff_trigger),
 	__ATTR(bootdev, 0444, show_bootdev, NULL),
 	__ATTR_NULL,
 };
@@ -112,8 +144,18 @@ static struct class odroid_class = {
 	.class_attrs = odroid_class_attrs,
 };
 
+static enum hrtimer_restart input_timer_function(struct hrtimer *timer)
+{
+	key_release_seconds = 0;
+	input_report_key(input_dev, KEY_POWER, 0);
+	input_sync(input_dev);
+
+	return HRTIMER_NORESTART;
+}
+
 static int odroid_sysfs_probe(struct platform_device *pdev)
 {
+	int error = 0;
 #ifdef CONFIG_USE_OF
 	struct device_node *node;
 
@@ -123,7 +165,42 @@ static int odroid_sysfs_probe(struct platform_device *pdev)
 #if defined(CONFIG_HAS_WAKELOCK)
 		wake_lock(&sleep_wake_lock);
 #endif
-	return 0;
+
+/***********************************************************************
+ * virtual key init (Power Off Key)
+***********************************************************************/
+	input_dev = input_allocate_device();
+	if (!input_dev) {
+		error = -ENOMEM;
+		goto err_out;
+	}
+
+	input_dev->name = "vt-input";
+	input_dev->phys = "vt-input/input0";
+	input_dev->id.bustype = BUS_HOST;
+	input_dev->id.vendor = 0x16B4;
+	input_dev->id.product = 0x0701;
+	input_dev->id.version = 0x0001;
+	input_dev->keycode = keycode;
+	input_dev->keycodesize = sizeof(keycode[0]);
+	input_dev->keycodemax = ARRAY_SIZE(keycode);
+
+	set_bit(EV_KEY, input_dev->evbit);
+	set_bit(KEY_POWER & KEY_MAX, input_dev->keybit);
+
+	error = input_register_device(input_dev);
+	if (error) {
+		input_free_device(input_dev);
+		goto err_out;
+	}
+
+	pr_info(KERN_INFO "%s input driver registered!!\n", "Virtual-Key");
+
+	hrtimer_init(&input_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	input_timer.function = input_timer_function;
+
+err_out:
+	return error;
 }
 
 static  int odroid_sysfs_remove(struct platform_device *pdev)
